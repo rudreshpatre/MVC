712f602b8d91bc22fba13d1c6559c0abfa7e6abf
Rudresh Patre <rudreshpatre@gmail.com>
2018-10-09 11:03:45 +0530

    Mvc fundamentals

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 "MoviesByReleaseDate",
                 "movies/released/{year}/{month}",
