386372da1bcbc5e24392672c1fd10ab03e578fe4
Rudresh Patre <rudreshpatre@gmail.com>
2018-10-17 00:44:37 +0530

    WorkingWithData

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -5,17 +5,29 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.ViewModels;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Custumers
         public ActionResult Index()
         {
             CustomersViewModel model = new CustomersViewModel()
             {
-                Customers = GetCustomers()
+                Customers = _context.Customers.Include(c=>c.MembershipType).ToList()
             };
             return View(model);
         }
@@ -23,37 +35,13 @@ namespace Vidly.Controllers
         [Route("Customers/Details/{id}")]
         public ActionResult Details(int id)
         {
-            List<CustomerDetails> details = GetCustomerDetails();
-
-            var customerDetails = details.Where(d => d.Id == id).Select(d => d).FirstOrDefault();
+            var customerDetails = _context.Customers.Where(d => d.Id == id).Select(d => d).FirstOrDefault();
             if (customerDetails == null)
             {
                 return new HttpNotFoundResult();
-            }
-            var model = new CustomerDetailsViewModel()
-            {
-                CustomerDetials = customerDetails
-            };
-            return View(model);
+            }          
+            return View(customerDetails);
         }
-
-        private static List<CustomerDetails> GetCustomerDetails()
-        {
-            return new List<CustomerDetails>()
-            {
-                new CustomerDetails{Id =1,Name = "Rudresh", Age = 10, Sex = "Male" },
-                new CustomerDetails{Id =2,Name = "Morena", Age = 24, Sex="Female"}
-            };
-        }
-
-        private static List<Customer> GetCustomers()
-        {
-            return new List<Customer>()
-            {
-                new Customer{ Id = 1, Name="Rudresh"},
-                new Customer{ Id = 2,Name = "Morena"},
-                new Customer{ Id = 3,Name = "Cute Girl"}
-            };
-        }
+       
     }
 }
--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public int MembershipTypeId { get; set; }
     }
 }
