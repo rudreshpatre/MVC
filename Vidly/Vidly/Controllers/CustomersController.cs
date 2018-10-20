75d6d64de02b46504548ea71c2695323735d683a
Rudresh Patre <rudreshpatre@gmail.com>
2018-10-20 15:02:47 +0530

    Added CSRF protection to Add customer form.

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -55,6 +55,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
